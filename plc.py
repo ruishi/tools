5fb2dcd35c1942a11a19e106421d7ae986c82181
ruishi <ganruishi@gmail.com>
2013-11-26 23:31:49 -0500

    add plc.py

---
+++ plc.py
@@ -0,0 +1,16 @@
+################################################################################
+#author: RD Galang
+#desc: takes directories as input and for each directory creates an MPC
+#      playlist consisting of all audio-files within
+#TODO: ...
+################################################################################
+
+import sys
+
+def main(argv):
+    for arg in argv:
+        
+
+
+if '__name__' == '__main__':
+    main(sys.argv[1:])
