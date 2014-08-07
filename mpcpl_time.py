07fe8a65451d8f4404e68167e01340f4c05dca33
ruishi <ganruishi@gmail.com>
2014-08-07 16:12:53 -0400

    calculate hours in addition to minutes and seconds
    mpcpl_time.py:
    - calculate hours as well as hours and minutes
    - change calculate_duration to calculate_duration_units() for more
      clarity
    - use an OrderedDict instead of a namedtuple. This is to make easier to
      print only relevant units (e.g., if a playlist duration is less than
      an hour, I don't want to see a printout that says 0 hours)
    - pull out the code in main and put them into functions. I decided it
      would be nicer this way as now you can quickly see what's going on in
      main(). Also, great for profiling.

--- mpcpl_time.py
+++ mpcpl_time.py
@@ -3,21 +3,27 @@ playlist."""
 
 import sys
 import os
-from collections import namedtuple
+from collections import OrderedDict
 
 from hsaudiotag import auto
 
-Duration = namedtuple('Duration', ['minutes', 'seconds'])
-
 def main(playlist_path):
-    playlist_dir_path = os.path.dirname(os.path.abspath(playlist_path))
+    audio_files = extract_filenames(playlist_path)
+    total_seconds = calculate_seconds(playlist_path, audio_files)
+    duration = calculate_duration_units(total_seconds)
+    print_duration(duration)
+
+def extract_filenames(playlist_path):
     with open(playlist_path) as f:
         filename_data = [line.strip() for line in f.readlines()
                          if 'filename' in line]
     # Each line with filename data has the following format:
     # <PlaylistTrackNumber>,filename,<filename or path>
     # So we just grab the relevant bit at the end:
-    audio_files = [line.split(',')[-1] for line in filename_data]
+    return [line.split(',')[-1] for line in filename_data]
+
+def calculate_seconds(playlist_path, audio_files):
+    playlist_dir_path = os.path.dirname(os.path.abspath(playlist_path))
     total_seconds = 0
     for audio_file in audio_files:
         # Sometimes the filename line just has the name
@@ -34,19 +40,27 @@ def main(playlist_path):
             else:
                 print("Could not locate file: {}".format(audio_file))
                 continue
-
-    duration = calculate_duration(total_seconds)
-    print("Playlist duration: {} minutes, {} seconds".format(duration.minutes,
-                                                             duration.seconds))
+    return total_seconds
 
 def get_seconds(audio_file):
     audio_file = auto.File(audio_file)
     return audio_file.duration
 
-def calculate_duration(total_seconds):
-    minutes = total_seconds // 60
-    seconds = total_seconds % 60
-    return Duration(minutes, seconds)
+def calculate_duration_units(total_seconds):
+    duration = OrderedDict()
+    total_minutes = total_seconds // 60
+    duration['hours'] =  total_minutes // 60
+    duration['minutes'] = total_minutes % 60
+    duration['seconds'] = total_seconds % 60
+    return duration
+
+
+def print_duration(duration):
+    print("Playlist duration:", end=' ')
+    duration_str  = ', '.join("{} {}".format(length, unit)
+                              for unit, length in duration.items()
+                              if length > 0)
+    print(duration_str)
 
 if __name__ == '__main__':
     main(sys.argv[1])
